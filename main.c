5e8c2a2d61fc3f815614823aafc89cb9db1f7d7f
Sakaisha <aishasakkary@outlook.com>
2023-10-13 12:34:54 -0700

    try

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include "main.h"
 
 /**
+ * execute_command - Execute a command using execve
+ * @command: The command to execute
+ */
+void execute_command(char *command)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+	}
+	else if (pid == 0)
+	{
+		char *argv[] = {command, NULL};
+
+		if (execve(command, argv, NULL) == -1)
+		{
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		do
+		{
+			if (waitpid(pid, &status, WUNTRACED) == -1)
+			{
+				perror("waitpid");
+				exit(EXIT_FAILURE);
+			}
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+}
+
+/**
  * main - Entry point of the program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
@@ -8,32 +46,50 @@
  */
 int main(int argc, char *argv[])
 {
+	char *command;
+	size_t len;
+	ssize_t read_bytes;
+
 	while (1)
 	{
 		write(1, "#cisfun$ ", 10);
 
-		char buffer[1024];
+		command = NULL;
+		len = 0;
 
-		if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+		read_bytes = getline(&command, &len, stdin);
+
+		if (read_bytes == -1) /* Error or end of file (Ctrl+D) */
 		{
-			write(1, "\n", 1);
+			if (feof(stdin)) /* End of file */
+			{
+				write(1, "\n", 1);
+				free(command);
+				break;
+			}
+			else
+			{
+				perror("getline");
+				free(command);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		if (command[read_bytes - 1] == '\n')
+		{
+			command[read_bytes - 1] = '\0';
+		}
+
+		if (strcmp(command, "exit") == 0)
+		{
+			free(command);
 			break;
 		}
 
-		size_t len = strlen(buffer);
+		execute_command(command);
 
-		if (len > 0 && buffer[len - 1] == '\n')
-		{
-			buffer[len - 1] = '\0';
-		}
-		if (strcmp(buffer, "exit") == 0)
-		{
-			break;
-		}
-
-		write(1, buffer, strlen(buffer));
-		write(1, "\n", 1);
+		free(command);
 	}
 
-	return (0);
+	return 0;
 }
--- main.h
+++ main.h
@@ -5,9 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <fcntl.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#endif
+void execute_command(char *command);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
