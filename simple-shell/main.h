3151a2cddbe4818282f094eb6dfd9d984f95bed9
Sakaisha <aishasakkary@outlook.com>
2023-10-17 03:44:26 +0300

    save

--- simple-shell/main.h
+++ simple-shell/main.h
@@ -1,3 +1,5 @@
+// main.h
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -18,22 +20,6 @@ void display_error(char *message);
 void display_prompt();
 ssize_t read_line(char *buffer, size_t size);
 
-char *builtin_str[] = {
-    "cd",
-    "exit",
-    "pwd",
-    "echo",
-    "environ"
-};
-
-int (*builtin_func[])(char **) = {
-    &cd_builtin,
-    &exit_builtin,
-    &pwd_builtin,
-    &echo_builtin,
-    &environ_builtin
-};
-
 int cd_builtin(char **args);
 int exit_builtin(char **args);
 int pwd_builtin(char **args);
@@ -42,8 +28,5 @@ int environ_builtin(char **args);
 int execute_builtin(char **args);
 
 void tokenize(char *input, char **tokens);
-void display_error(char *message);
-void display_prompt();
-ssize_t read_line(char *buffer, size_t size);
 
 #endif /* MAIN_H */
