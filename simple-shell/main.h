7a3b84c724e55e25bd5e1de2fd1ce40e2ac25c26
Sakaisha <aishasakkary@outlook.com>
2023-10-17 03:46:10 +0300

    save

--- simple-shell/main.h
+++ simple-shell/main.h
@@ -20,6 +20,7 @@ void display_error(char *message);
 void display_prompt();
 ssize_t read_line(char *buffer, size_t size);
 
+int execute(char **args);
 int cd_builtin(char **args);
 int exit_builtin(char **args);
 int pwd_builtin(char **args);
