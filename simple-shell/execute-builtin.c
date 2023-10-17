7740c6410c5ba6ee5a063ca964244ce20bfa54b0
Sakaisha <aishasakkary@outlook.com>
2023-10-17 19:36:10 +0300

    ss

--- simple-shell/execute-builtin.c
+++ simple-shell/execute-builtin.c
@@ -8,14 +8,14 @@ char *get_path(char *command) {
     }
 
     char *token = strtok(path_copy, ":");
-    size_t written;
+    size_t token_len;
 
     while (1) {
         if (token == NULL) {
             break;
         }
 
-        size_t token_len = strlen(token);
+        token_len = strlen(token);
         size_t command_len = strlen(command);
         size_t path_len = token_len + command_len + 2;
 
@@ -26,14 +26,14 @@ char *get_path(char *command) {
             return NULL;
         }
 
-        written = write(STDOUT_FILENO, token, token_len);
+        size_t written = write(STDOUT_FILENO, token, token_len);
         written += write(STDOUT_FILENO, "/", 1);
         written += write(STDOUT_FILENO, command, command_len);
         write(STDOUT_FILENO, "\0", 1);
 
         if (access(path, X_OK) == 0) {
             free(path_copy);
-            free(path);  // Free the allocated path before returning
+            free(path);
             return path;
         }
 
@@ -66,9 +66,6 @@ void execute_command(char **argv) {
 }
 
 int execute_builtin(char **args) {
-
-int execute_builtin(char **args) {
-
     if (args[0] == NULL) {
         return 1;
     }
@@ -88,6 +85,7 @@ int execute_builtin(char **args) {
         int status;
 
         pid = fork();
+
         if (pid == 0) {
             if (execvp(args[0], args) == -1) {
                 write(STDERR_FILENO, "hsh: command not found: ", 24);
