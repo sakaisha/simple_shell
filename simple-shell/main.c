cc4a7a53a6d87b0d718b16dee20d0cb7d304b444
Sakaisha <aishasakkary@outlook.com>
2023-10-17 18:29:34 +0300

    ss

--- simple-shell/main.c
+++ simple-shell/main.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 int main(int argc, char *argv[]) {
-    if (argc == 1) {
-        char *input = NULL;
-        size_t len = 0;
-        ssize_t read;
+    char *input = NULL;
+    size_t len = 0;
+    ssize_t read;
 
+    if (argc == 1) {
         while (1) {
+            char *tokens[MAX_TOKENS];
             display_prompt();
             if ((read = getline(&input, &len, stdin)) == -1) {
                 break; /* End of file (Ctrl+D) detected */
             }
 
-            char *tokens[MAX_TOKENS];
             tokenize(input, tokens);
 
             if (tokens[0] != NULL) {
@@ -21,18 +21,12 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-
-        free(input);
     } else if (argc == 2) {
         FILE *file = fopen(argv[1], "r");
         if (file == NULL) {
             perror("Error opening file");
             return EXIT_FAILURE;
         }
-
-        char *input = NULL;
-        size_t len = 0;
-        ssize_t read;
 
         while ((read = getline(&input, &len, file)) != -1) {
             char *tokens[MAX_TOKENS];
@@ -46,11 +40,11 @@ int main(int argc, char *argv[]) {
         }
 
         fclose(file);
-        free(input);
     } else {
         perror("Usage: hsh [file]");
         return EXIT_FAILURE;
     }
 
+    free(input);
     return EXIT_SUCCESS;
 }
