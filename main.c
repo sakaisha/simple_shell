166115f69548f62b663565eb65fd7a30ec1e07ce
Sakaisha <aishasakkary@outlook.com>
2023-10-18 04:12:02 +0300

    main.c

--- main.c
+++ main.c
@@ -1,56 +1,86 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 100
 
 void display_prompt(void) {
     const char prompt[] = "$ ";
     write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
 }
 
-int main(int argc, char *argv[]) {
-    char *input = NULL;
-    size_t len = 0;
-    ssize_t read;
-
-    if (argc == 1 || (argc == 2 && strcmp(argv[1], "-c") == 0)) {
-        while (1) {
-            char *tokens[MAX_TOKENS];
-            display_prompt();
-            if ((read = read_line(&input, &len, stdin)) == -1) {
-                break; /* End of file (Ctrl+D) detected */
-            }
-
-            tokenize(input, tokens);
-
-            if (tokens[0] != NULL) {
-                if (execute_builtin(tokens) == -1) {
-                    perror("Execution failed");
-                }
-            }
-        }
-    } else if (argc == 2 && strcmp(argv[1], "-c") != 0) {
-        FILE *file = fopen(argv[1], "r");
-        if (file == NULL) {
-            perror("Error opening file");
-            return EXIT_FAILURE;
-        }
-
-        while ((read = read_line(&input, &len, file)) != -1) {
-            char *tokens[MAX_TOKENS];
-            tokenize(input, tokens);
-
-            if (tokens[0] != NULL) {
-                if (execute_builtin(tokens) == -1) {
-                    perror("Execution failed");
-                }
-            }
-        }
-
-        fclose(file);
+char* string_token(char* str, const char* delim) {
+    static char* save_ptr;
+    char* token;
+    if (str != NULL) {
+        save_ptr = str;
     } else {
-        perror("Usage: hsh [-c command | script-file]");
-        return EXIT_FAILURE;
+        if (save_ptr == NULL || *save_ptr == '\0') {
+            return NULL;
+        }
     }
+    while (*save_ptr != '\0' && strchr(delim, *save_ptr) != NULL) {
+        save_ptr++;
+    }
+    if (*save_ptr == '\0') {
+        return NULL;
+    }
+    token = save_ptr;
+    while (*save_ptr != '\0' && strchr(delim, *save_ptr) == NULL) {
+        save_ptr++;
+    }
+    if (*save_ptr != '\0') {
+        *save_ptr = '\0';
+        save_ptr++;
+    }
+    return token;
+}
 
-    free(input);
-    return EXIT_SUCCESS;
+void executeCommand(char *command) {
+    pid_t pid = fork();
+    char *envp[] = {NULL}; 
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) { 
+        char *args[MAX_COMMAND_LENGTH];
+        char *token = string_token(command, " \t\n");
+        int i = 0;
+        while (token != NULL) {
+            args[i++] = token;
+            token = string_token(NULL, " \t\n");
+        }
+        args[i] = NULL;
+        if (execve(args[0], args, envp) == -1) {
+            perror("hsh");
+            exit(EXIT_FAILURE);
+        }
+    } else { 
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+    while (1) {
+        display_prompt();
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            perror("fgets");
+            exit(EXIT_FAILURE);
+        }
+        command[strcspn(command, "\n")] = '\0';
+        if (strcmp(command, "exit") == 0) {
+            printf("Exiting the shell.\n");
+            break;
+        }
+        executeCommand(command);
+    }
+    return 0;
 }
