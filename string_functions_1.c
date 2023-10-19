9d58dfa05bc4bc934151fe6f38479080efc755f5
Sakaisha <aishasakkary@outlook.com>
2023-10-19 04:55:01 +0300

    one last change

--- string_functions_1.c
+++ string_functions_1.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char c, unsigned int n)
 {
 	int i = 0;
 
 	for (; n > 0; i++)
 	{
-		s[i] = b;
+		s[i] = c;
 		n--;
 	}
 	return (s);
