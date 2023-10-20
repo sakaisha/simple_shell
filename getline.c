67a33fa37dbb68361b6b052e0b7f059789082801
Sakaisha <aishasakkary@outlook.com>
2023-10-20 11:49:03 -0700

    for betty and alx

--- getline.c
+++ getline.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
  * _getline_trial - trial to implement getline
  * @buff: Pointer to the buffer
  * @n: Pointer to the size of the buffer
  * @stream: Pointer to the stream.
+ *
  * Return: Number of characters read (including newline), or -1 on failure.
  */
 ssize_t _getline_trial(char **buff, size_t *n, FILE *stream)
 {
-	size_t pos = 0, i = 0;
+	size_t pos = 0;
 	char c;
-	int fd = _fileno(stream);
 	int ignore_line = 0;
+	int word_started = 0;
 
 	if (buff == NULL || n == NULL)
 		return (-1);
@@ -23,40 +25,86 @@ ssize_t _getline_trial(char **buff, size_t *n, FILE *stream)
 		if (*buff == NULL)
 			return (-1);
 	}
-	while (read(fd, &c, 1) > 0)
+	while (read_char_from_stream(&c, stream) > 0)
 	{
-		if (!ignore_line)
-		{
-			if (pos + 1 >= *n)
-			{
-				size_t new_size = *n * 2;
-				char *new_buff = (char *)malloc(new_size);
+		ssize_t result = build_buffer(buff, n, c, &pos, &ignore_line, &word_started);
 
-				if (new_buff == NULL)
-					return (-1);
-				for (i = 0; i < *n; ++i)
-					new_buff[i] = (*buff)[i];
-				free(*buff);
-				*buff = new_buff;
-				*n = new_size;
-			}
-			(*buff)[pos++] = c;
-		}
-
-		if (c == '#')
-			ignore_line = 1;
-
-		if (c == '\n')
-		{
-			ignore_line = 0;
+		if (result == 1)
 			break;
-		}
+		if (result == -1)
+			return (-1);
 	}
-
 	if (pos == 0)
 		return (-1);
+
 	(*buff)[pos] = '\0';
 	return ((ssize_t)pos);
+}
+
+/**
+ * build_buffer - Processes characters and builds the buffer.
+ * @buff: Pointer to the buffer.
+ * @n: Pointer to the buffer size.
+ * @c: Current character.
+ * @pos: Pointer to the current position in the buffer.
+ * @ignore_line: Pointer to the flag indicating whether to ignore the line.
+ * @word_started: Pointer to the flag indicating whether a word has started.
+ *
+ * Return: 0 on success, -1 on failure
+ */
+ssize_t build_buffer(char **buff, size_t *n, char c,
+		size_t *pos, int *ignore_line, int *word_started)
+{
+	size_t i;
+
+	if (!(*ignore_line))
+	{
+		if (*pos + 1 >= *n)
+		{
+			size_t new_size = *n * 2;
+			char *new_buff = (char *)malloc(new_size);
+
+			if (new_buff == NULL)
+				return (-1);
+			for (i = 0; i < *n; ++i)
+				new_buff[i] = (*buff)[i];
+			free(*buff);
+			*buff = new_buff;
+			*n = new_size;
+		}
+		(*buff)[(*pos)++] = c;
+	}
+
+	if (c == ' ')
+		*word_started = 0;
+	else if (c == '#')
+	{
+		if (!(*word_started))
+			*ignore_line = 1;
+	}
+	else
+		*word_started = 1;
+
+	if (c == '\n')
+	{
+		*ignore_line = 0;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * read_char_from_stream - Reads a character from the stream.
+ * @c: Pointer to store the read character.
+ * @stream: File stream.
+ *
+ * Return: 0 on success, -1 on failure
+ */
+ssize_t read_char_from_stream(char *c, FILE *stream)
+{
+	int fd = _fileno(stream);
+
+	return (read(fd, c, 1));
 }
 
 /**
@@ -71,4 +119,3 @@ int _fileno(FILE *stream)
 		return (-1);
 	return (stream->_fileno);
 }
-
