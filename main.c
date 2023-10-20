caae42980c0338f7064fe5211b3de5ddff72d2cc
Sakaisha <aishasakkary@outlook.com>
2023-10-20 07:37:56 -0700

    update return in main

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(int argc, char **argv, char **env)
 	size_t size = 0;
 	ssize_t line_st;
 	int count = 0, spaces;
+	int status = 0;
 
 	(void)argc;
 	_memset(dir, 0, BUFFER_SIZE);
@@ -33,14 +34,14 @@ int main(int argc, char **argv, char **env)
 		else if ((line_st == -1) && (spaces != 2))
 			break;
 
-		execute(buff, line_st, name, count, env);
+		status = execute(buff, line_st, name, count, env);
 	}
 
 	if (buff)
 		free(buff);
 
 	handle_newline();
-	return (0);
+	return (status);
 }
 
 
@@ -95,3 +96,4 @@ void handle_newline(void)
 	if (isatty(0) == 1)
 		write(1, "\n", 1);
 }
+
